7d28e9bf6c89a18a6769eaf697680877681052ac
DEsimas <simasuh@gmail.com>
2023-06-15 19:36:18 +0300

    setCommands script

---
+++ src/utils/setCommands.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const commands = [
+  {
+    command: 'ping',
+    description: 'pongs'
+  },
+  {
+    command: 'hello',
+    description: 'hellos'
+  }
+];
+
+const bot = new TelegramBot(process.env.TOKEN, {polling: true});
+
+bot.setMyCommands(commands)
+  .then(() => {
+    console.log('Success');
+    bot.getMyCommands()
+      .then(console.log)
+  })
+  .catch((e) => {
+    console.log(`Failed to set commands\n${e}`);
+  });
